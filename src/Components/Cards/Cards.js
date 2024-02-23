e6096991bb69e6ca2fa1f1290ce8ac48f41d876e
Ahmedntc <ahmudi.ab@gmail.com>
2024-02-23 00:45:01 -0300

    big card duplo feito

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -5,25 +5,39 @@ import "./Cards.css";
 
 function Cards() {
     return (
-        <div className="Cards">
+        <section className="Cards">
             <div className="double-card">
-                <div className="card-burger">
-                    <a className="card-link" href ="#">
-                        <div className="card-content">
-                            <p className="card-title">Restaurante</p>
-                            <figure className="card-img">
-                                <img src="https://static.ifood-static.com.br/image/upload/f_auto/webapp/landingV2/restaurant.png" alt="burger" />   
-                            </figure>
-                            <p className="card-text">Ver opções</p>
-                        </div>
-                    </a>
-                </div>
+                <div className="maincards">
+                    <div className="bigcard">
+                        <a className="burger">
+                            <div className="container-bigcard">
+                                <p className="bigcard-title">Restaurante</p>
+                                <figure className="img-container">
+                                    <img className="img" src="https://static.ifood-static.com.br/image/upload/f_auto/webapp/landingV2/restaurant.png" alt="burger" />
+                                </figure>
+                                <div className="btn-like">Ver opções</div>
+                            </div>
+                        </a>
+                    </div>
 
-                <div className="card-cart">
-
+                <div className="bigcard--cart">
+                        <a className="cart">
+                            <div className="container-bigcard">
+                                <p className="bigcard-title">Mercado</p>
+                                <figure className="img-container">
+                                    <img className="img" src="https://static.ifood-static.com.br/image/upload/f_auto/webapp/landingV2/market.png" alt="mercado" />
+                                </figure>
+                                <div className="btn-like--cart">Buscar lojas</div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="card-carousel">
+
+            </div>
+
+        </section>
 
     );
 }
