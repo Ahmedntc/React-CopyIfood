552920caa5deff1c0aa61fa993e5ec4d1bc3b744
Ahmedntc <ahmudi.ab@gmail.com>
2024-02-23 01:08:49 -0300

    small cards carousel

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -19,7 +19,6 @@ function Cards() {
                             </div>
                         </a>
                     </div>
-
                 <div className="bigcard--cart">
                         <a className="cart">
                             <div className="container-bigcard">
@@ -34,6 +33,26 @@ function Cards() {
                 </div>
             </div>
             <div className="card-carousel">
+                <a className="small-card">
+                    <figure className="small-link--drinks">
+                        <img className="img-drinks" src="https://static.ifood-static.com.br/image/upload/f_auto/webapp/landingV2/drinks.png" alt="drinks" />
+                    </figure>
+                    <p className="small-title">Bebidas</p>
+                </a>
+                <a className="small-card">
+                    <figure className="small-link--farm">
+                        <img className="img-farm" src="https://static.ifood-static.com.br/image/upload/f_auto/webapp/landingV2/pharmacy.png" alt="farm" />
+
+                    </figure>
+                    <p className="small-title">Farmácia</p>
+                </a>
+                <a className="small-card">
+                    <figure className="small-link--pet">
+                        <img className="img-pet" src="https://static.ifood-static.com.br/image/upload/f_auto/webapp/landingV2/petshop.png" alt="pet" />
+
+                    </figure>
+                    <p className="small-title">Petshop</p>
+                </a>
 
             </div>
 
